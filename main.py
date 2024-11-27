7b761d2e8ae63ab0799af7305c26da7cd98a91a7
neatsolin <neatsolin98@gmail.com>
2024-11-27 09:13:33 +0700

    solin

--- main.py
+++ main.py
@@ -74,6 +74,8 @@ def condiction_2(message2):
         return "html element"
     elif "code block" in text:
         return "code block"
+    elif "heading" in text:
+        return"heading"
     return None
 
 #def_second_function for python
